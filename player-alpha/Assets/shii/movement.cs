aed52b147fd91e337aba766d7d04c772d130e205
MechanizedFemboy <KaiserAkkumulator@gmail.com>
2025-11-21 23:20:32 +0300

    player alpha

---
+++ player-alpha/Assets/shii/movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 25f;
+    private Rigidbody2D rb;
+    public float n = 25f;
+    public BoxCollider2D groundcheck;
+    public LayerMask groundmask;
+    public bool isgrounded;
+
+    bool checkground()
+    {
+        isgrounded = Physics2D.OverlapAreaAll(groundcheck.bounds.min, groundcheck.bounds.max, groundmask).Length > 0;
+        return isgrounded;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+
+    
+    
+
+    private Vector2 mostream;
+    void Update()
+    {
+        mostream.x = Input.GetAxisRaw("Horizontal") * speed;
+        rb.velocity = new Vector2(mostream.x, rb.velocity.y);
+
+        if (Input.GetButton("Jump")){
+            if(checkground()){rb.AddForce(Vector2.up * n);}
+        }
+    }
+}
