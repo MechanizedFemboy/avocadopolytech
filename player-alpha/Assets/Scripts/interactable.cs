89b72317030e51da342269fb33b2713bb89b02fe
MechanizedFemboy <KaiserAkkumulator@gmail.com>
2025-12-07 22:16:41 +0300

    wew3rg

--- player-alpha/Assets/Scripts/interactable.cs
+++ player-alpha/Assets/Scripts/interactable.cs
@@ -1,1 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Interactable : MonoBehaviour
+{
+    public string ID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
+    }
+}
