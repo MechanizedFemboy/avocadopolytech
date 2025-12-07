123077c1bab842a64bfcea2ef163ac5985e91c99
MechanizedFemboy <KaiserAkkumulator@gmail.com>
2025-12-07 15:00:36 +0300

    восстанавливаю свои файлы часть 1

--- player-alpha/Assets/Scripts/movement.cs
+++ player-alpha/Assets/Scripts/movement.cs
@@ -9,13 +9,13 @@ public class movement : MonoBehaviour
     public float speed = 25f;
     private Rigidbody2D rb;
     public float n = 25f;
-    public BoxCollider2D groundcheck;
     public LayerMask groundmask;
     public bool isgrounded;
-    PC pc = new PC();
     private Camera cam;
     public Vector2 BoxSize;
     public float dist;
+    private Animator anim;
+    private SpriteRenderer sprt;
 
     bool checkground()
     {
@@ -40,12 +40,16 @@ public class movement : MonoBehaviour
     {
         cam = Camera.main;
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprt = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-
-    
-    
+    void Animate()
+    {
+        bool IsMoving = Input.GetAxisRaw("Horizontal") != 0;
+        sprt.flipX = Input.GetAxisRaw("Horizontal") < 0;
+        anim.SetBool("IsMoving", IsMoving);
+    }
 
     private Vector2 mostream;
     void Update()
@@ -53,51 +57,13 @@ public class movement : MonoBehaviour
         mostream.x = Input.GetAxisRaw("Horizontal") * speed;
         rb.velocity = new Vector2(mostream.x, rb.velocity.y);
 
-        if (Input.GetButton("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             if (checkground()) { rb.AddForce(Vector2.up * n); }
         }
 
-
-        /*if (Input.GetMouseButtonDown(0)) // при нажатии левой кнопки мыши
-
-        if (Input.GetMouseButtonDown(0))
- 827cbe9c68ef5bfc1367b3661448beadcdfaac82
-        {
-
-            Vector2 mp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            Debug.Log(mp);
-
-            if (pc.ClicktedHZ(mp))
-            {
-                Instantiate(pc.screen);
-            }
-        }*/
-
-        // if (Input.GetMouseButtonDown(0)) // пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
-        // {
-        //     Vector2 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //     Debug.Log(mousePos);
-        /*3758d021cc1ada4108d23d5c3fb2ad9811ebbbe2*/
-        //     //Vector2 mp = cam.ScreenToWorldPoint(Input.mousePosition);
-
-
-        //     //if (pc.ClicktedHZ(mp))
-        //     //{
-
-        //     //}
-        //     // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-
-        // }
-
-
-
-        //ne pishi kommentarii kirillitsey, git stiraet ikh kogda pushaet
-
-
- 
+        Animate();
     }
 }
+
 
